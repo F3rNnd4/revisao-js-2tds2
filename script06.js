ede3f661f208a5be081e181840f2810a41be9876
Fernanda Louro <fernandaalves.tds2.senai@gmail.com>
2024-08-08 17:15:49 -0300

    feat: resolve books exercise

---
+++ script06.js
@@ -0,0 +1,7 @@
+let metaLivros = 6;
+
+let livrosLidos = ["O castelo animado", "Homem-Aranha no Aranhaverso", "A voz do silêncio", "Your name", "Fabricante de lágrimas", "Soul Eater"];
+
+for (let i = 0; i < livrosLidos.length; i++) {
+    console.log(livrosLidos[i]);
+}
